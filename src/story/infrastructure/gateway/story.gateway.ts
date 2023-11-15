7c7fff531603fb0acf635876a7b487e57cb98e91
Wilfer Daniel Ciro Maya <ciro@DESKTOP-RLBAN4Q>
2023-11-15 15:50:45 -0500

    feature: add forward messages

--- src/story/infrastructure/gateway/story.gateway.ts
+++ src/story/infrastructure/gateway/story.gateway.ts
@@ -28,42 +28,11 @@ export class StoryGateway {
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('searchHistories')
-  async handleBuscarLibros(@MessageBody() chatMessage: string): Promise<void> {
+  @SubscribeMessage('sendMessage')
+  async handleBuscarLibros(
+    @MessageBody() chatMessage: { sender: string; message: string },
+  ): Promise<void> {
     console.log(chatMessage);
-    const stories = await this.service.findAll();
-    this.server.emit(
-      'resultadosBusqueda',
-      stories.map((story) => this.mapper.toDto(story)),
-    );
-    /*try {
-      const responseChatGPT = await axios.post(
-        'https://api.openai.com/v1/engines/davinci-codex/completions',
-        {
-          prompt: `Encuentra libros sobre ${chatMessage}`,
-          max_tokens: 150,
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer tu_clave_de_API_de_OpenAI`,
-          },
-        },
-      );
-
-      const foundBooks = this.processAnswer(
-        responseChatGPT.data.choices[0].text,
-      );
-      this.server.emit('resultadosBusqueda', foundBooks);
-    } catch (error) {
-      console.error(error);
-    }*/
-  }
-
-  private processAnswer(respuesta: string): any[] {
-    // Analizar la respuesta de ChatGPT y buscar libros en la colección de libros
-    // Devolver los libros encontrados como un arreglo de objetos
-    // { titulo, autor, contenido, palabrasClave }
-    return [respuesta];
+    this.server.emit('responseMessage', chatMessage);
   }
 }
