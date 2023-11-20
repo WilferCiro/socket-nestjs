6e8fe4003802faa7153f81f2f68db65e3f16a111
Wilfer Daniel Ciro Maya <ciro@DESKTOP-RLBAN4Q>
2023-11-20 16:19:03 -0500

    feature: add web push notifications

--- src/notifications/infrastructure/controllers/notifications.controller.ts
+++ src/notifications/infrastructure/controllers/notifications.controller.ts
@@ -15,6 +15,9 @@ export class NotificationsController {
     // Almacena la suscripción en tu base de datos o donde prefieras
     // Normalmente se utilizaría una base de datos para manejar múltiples suscripciones
     // Aquí se almacena en una variable de clase solo con fines de demostración
-    NotificationsService.subscriptions.push(subscription);
+    const currentSubs = NotificationsService.subscriptions;
+    if (currentSubs.find((sub) => sub.auth === subscription.auth) === -1) {
+      NotificationsService.subscriptions.push(subscription);
+    }
   }
 }
